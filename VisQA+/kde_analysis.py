b47d6771596d500438fab927a949f6bf98b7407b
Yao Wang <yao.wang@vis.uni-stuttgart.de>
2022-02-23 16:26:17 +0100

    feat: step 5 added (but seems like still have some bug)

--- VisQA+/kde_analysis.py
+++ VisQA+/kde_analysis.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
+from PIL import Image
 import matplotlib.pyplot as plt
 
 from VisQA.preprocessing.parser.parse_element_labels import parse_element_label, combine_rows
 from VisQA.dataset.dataset_io import convert_desc
 from VisQA.preprocessing.match.match_eye_fixations_to_element_labels import compute_is_in_polygon
 from sklearn.neighbors import KernelDensity
+from skimage.draw import polygon
 
 
 def get_hit_aoi(element_labels, xp, yp):
@@ -35,14 +37,17 @@ def check_consistency(fix, gaze_samples):
 
 if __name__ == '__main__':
     img_src = '_Di4yk2H64QEBkEncRGMhg==.0'
+    img_dir = '/netpool/homes/wangyo/Dataset/VisQA/merged/src/'
 
-    fixation_path = f'C:/Users/kochme/Datasets/VisQA/eyetracking/csv_files/fixationsByVis/{img_src}/enc/byq1.csv'
+    #fixation_path = f'C:/Users/kochme/Datasets/VisQA/eyetracking/csv_files/fixationsByVis/{img_src}/enc/byq1.csv'
+    fixation_path = f'/netpool/homes/wangyo/Dataset/VisQA/eyetracking/csv_files/fixationsByVis/{img_src}/enc/byq1.csv'
     fixation = pd.read_csv(fixation_path)
 
-    element_labels = parse_element_label(f'C:/Users/kochme/Datasets/VisQA/element_labels/{img_src}')
+    element_labels = parse_element_label(f'/netpool/homes/wangyo/Dataset/VisQA/element_labels/{img_src}')
     element_labels = combine_rows(element_labels)
+    #print(element_labels)
 
-    bandwidth = 1.0
+    bandwidth = 1
 
     for index, row in fixation.iterrows():
         # Gaze Left XY
@@ -50,3 +55,34 @@ if __name__ == '__main__':
 
         # Step 4: Perform KDE on gaze samples associated to fixation
         kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(gaze_samples)
+
+        # Step 5: check which AOIs are overlaid by the resulting density and to which extent
+        with Image.open(img_dir+img_src+'.png') as im:
+            #fig = plt.figure(figsize=(16, 16))
+            #plt.imshow(im)
+            width, height = im.size  # original image size
+
+            X,Y = np.meshgrid(np.arange(0, width, 1), np.arange(0, height, 1))
+            xy = np.vstack([Y.ravel(), X.ravel()]).T
+
+            Z = np.full((width, height), 0, dtype="int")
+            Z = np.exp(kde.score_samples(xy))
+            Z = Z.reshape(X.shape)
+
+            for row in element_labels.iterrows():
+                #row[1][0] id
+                #row[1][1] desc
+                #row[1][2] file
+                #row[1][3] coordinates
+                rr, cc = polygon(
+                    np.array(row[1][3])[:,1],
+                    np.array(row[1][3])[:,0],
+                    (width, height))
+                try:
+                    print(rr.max(), cc.max())
+                    print(Z[rr,cc].sum()) # this should be the accumulated density of all pixels within that polygon
+                except: break
+
+            #levels = np.linspace(0, Z.max(), 25)
+            #plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
+            #plt.show()
