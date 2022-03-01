98ebc5c5a391840052aa99a6f64b5aa57b3fe09d
Maurice <maurice.koch@visus.uni-stuttgart.de>
2022-03-02 00:40:32 +0100

    Flipping candidate computation only for 2,3,4 overlap

--- VisQA+/flipping_rate_analysis.py
+++ VisQA+/flipping_rate_analysis.py
@@ -14,7 +14,17 @@ import seaborn as sns
 from glob import glob
 
 
-def flipping_candidate_score(densities):
+def parse_densities(file):
+    densities = json.load(file).values()
+    for ds in densities:
+        sum_densities = sum([d for _, d in ds])
+        if sum_densities < 1.:
+            ds.append(('#', 1.-sum_densities))
+        #assert sum_densities <= 1+1e-1, f"Element labels are overlapping. Densities add up to {sum_densities:.3f}"
+    return densities
+
+
+def flipping_candidate_score_of_rank(densities, r):
     """
     Flipping candidates score has the following interpretation:
     ~> 0: The density distribution is peaked, i.e. the fixation mostly covers just a single AOI.
@@ -26,16 +36,29 @@ def flipping_candidate_score(densities):
         uniform = 1 / N
         return sum([abs(d - uniform) for _, d in densities])
 
-    sorted_densities = sorted(densities, reverse=True, key=lambda x: x[1])
     N = len(densities)
-    max_score = 0
+    copy = list(densities)
 
-    # Flipping candidates switch between at least two AOIs
-    for n in range(2, N+1):
-        deviation = deviation_from_uniform(sorted_densities[:n], n)
-        score = 1. - ((2 / n) * deviation)
-        max_score = max(score, max_score)
-    return max_score
+    # Add dummy zeros when rank is larger than number density entries.
+    if r > N:
+        copy.extend([('0', 0)] * (r - N))
+
+    copy.sort(reverse=True, key=lambda x: x[1])
+    deviation = deviation_from_uniform(copy[:r], r)
+    score = 1. - ((2 / r) * deviation)
+    return score
+
+
+def flipping_candidate_score(densities, ranks=(2, 3, 4)):
+    """
+    Flipping candidates score has the following interpretation:
+    ~> 0: The density distribution is peaked, i.e. the fixation mostly covers just a single AOI.
+    ~> 1: The density distribution is close to uniform, i.e. the fixation covers at least two AOI to a very similar extent.
+
+    NOTE: Is there off-the-shelf solution for this? There might be better / more elegant way to compute this.
+    """
+    scores = [flipping_candidate_score_of_rank(densities, r) for r in ranks]
+    return max(scores)
 
 
 def find_flipping_candidates(fixation_densities, threshold):
@@ -44,19 +67,13 @@ def find_flipping_candidates(fixation_densities, threshold):
     Output can be verified with show_density_overlay=True
     """
     flipping_candidates = []
-
     for densities in fixation_densities:
-        sum_densities = sum([d for _, d in densities])
-        if sum_densities < 1.:
-            densities.append(('#', 1.-sum_densities))
-        #assert sum_densities <= 1+1e-1, f"Element labels are overlapping. Densities add up to {sum_densities:.3f}"
-
         # Step 6: check for which segments the distribution overlays at least two AOIs to a very similar extent (the flipping candidates)
-        if len(densities) > 1:
-            score = flipping_candidate_score(densities)
-            logging.info(f'  Flipping candidate score = {score:.3f}: {densities}')
-            if score >= threshold:
-                flipping_candidates.append((densities))
+        score = flipping_candidate_score_of_rank(densities, r=10)
+        logging.info(f'  Flipping candidate score = {score:.3f}: {densities}')
+        if score >= threshold:
+            flipping_candidates.append((densities))
+
     return flipping_candidates
 
 
@@ -67,8 +84,7 @@ def fcr_of_vis(densities_dir, flipping_threshold):
     flipping_candidate_rate = []
     for path in glob(os.path.join(densities_dir, '*.json')):
         with open(path, 'r') as f:
-            densities = json.load(f).values()
-
+            densities = parse_densities(f)
             flipping_candidates = find_flipping_candidates(densities, flipping_threshold)
             flipping_candidate_rate.append(len(flipping_candidates) / len(densities))
             logging.info(f'\nNumber of fixations being flipping candidates: {len(flipping_candidates)}/{len(densities)}\n')
@@ -94,7 +110,7 @@ if __name__ == '__main__':
     sns.set()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_dir", type=str, default=None)
+    parser.add_argument("--dataset_dir", type=str, required=True)
     parser.add_argument("--vis_types", choices=VIS_TYPES, nargs='+', default=VIS_TYPES)
     args = vars(parser.parse_args())
     vis_types = set(args['vis_types'])
@@ -102,16 +118,17 @@ if __name__ == '__main__':
     logging.basicConfig(filename='flipping_rate_analysis.log', filemode='w', level=logging.INFO)
     type2rate = {vt: [] for vt in vis_types}
 
-    for fc_threshold in np.linspace(0, 1, 10):
+    for fc_threshold in np.linspace(0, 1, 100):
         for vis_type in args['vis_types']:
             type_rates = fcr_of_vis_type(vis_type, args['dataset_dir'], fc_threshold)
             type2rate[vis_type].append(np.mean(type_rates))
 
     for vis_type, rates in type2rate.items():
-        plt.plot(np.linspace(0, 1, 10), rates, label=vis_type)
+        plt.plot(np.linspace(0, 1, 100), rates, label=vis_type)
 
     plt.xlabel('Flipping candidate threshold')
     plt.ylabel('Flipping candidate rate')
+    plt.title('Flipping candidate rate per vis type')
     plt.legend()
-    plt.grid(axis='y')
+    plt.grid(axis='both')
     plt.show()
