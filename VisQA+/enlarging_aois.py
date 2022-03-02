a3eeeb462a24df9e655e225dcacab557189d9390
Yao Wang <yao.wang@vis.uni-stuttgart.de>
2022-03-02 14:34:12 +0100

    fix: enlarging_AOIS coordinate >0 <width/height, change rate to absolute width

--- VisQA+/enlarging_aois.py
+++ VisQA+/enlarging_aois.py
@@ -46,15 +46,20 @@ def calc_shrink_width(polygon: np.array, r):
     area = Area(polygon)
     perimeter = Perimeter(polygon)
     L = area * (1 - r ** 2) / perimeter
+
     return L if r > 0 else -L
 
-def shrink_polygon(polygon: np.array, r):
+def shrink_polygon(polygon: np.array, L, imgsize, fixWidth=False):
     N, d = polygon.shape
+    width, height = imgsize
     if N < 3 or d != 2:
         raise ValueError
 
     shrinked_polygon = []
-    L = calc_shrink_width(polygon, r)
+
+    if not fixWidth:
+        L = calc_shrink_width(polygon, L)
+    
     for i in range(N):
         Pi = polygon[i]
         v1 = polygon[i-1] - Pi
@@ -66,12 +71,14 @@ def shrink_polygon(polygon: np.array, r):
         sin_theta = np.abs(np.cross(normalize_v1, normalize_v2))
 
         Qi = Pi + L / sin_theta * (normalize_v1 + normalize_v2)
-        shrinked_polygon.append(Qi)
+        Qi = np.maximum(Qi, 0)
+        Qi[0] = np.where(Qi[0] < width - 1, Qi[0], width - 1)
+        Qi[1] = np.where(Qi[1] > height - 1, Qi[1], height - 1)
+        shrinked_polygon.append(np.array(Qi, dtype=int))
     return np.asarray(shrinked_polygon)
 
-def enlargeAOI(element_labels, im, ratio:float):
-    df = pd.DataFrame(columns=['id', 'desc', 'file', 'x', 'y'])
-    width, height = im.size
+def enlargeAOI(element_labels, im, enlargeL:float):
+    df = pd.DataFrame(columns=['id', 'desc', 'x', 'y'])
 
     for row in element_labels.iterrows():
         #row[1][0] id
@@ -86,14 +93,15 @@ def enlargeAOI(element_labels, im, ratio:float):
         perimeter = Perimeter(poly)
         area = Area(poly)
 
-        expansion_poly = shrink_polygon(poly, ratio)
+        expansion_poly = shrink_polygon(poly, enlargeL, im.size, fixWidth=True)
         #print(perimeter, area, expansion_poly)
 
         for i in range(np.shape(expansion_poly)[0]):
-            df = df.append({'id': row[1][0], 'desc': row[1][1], 'file': row[1][2], 'x': expansion_poly[i][0], 'y': expansion_poly[i][1]}, ignore_index=True)
+            df = df.append({'id': row[1][0], 'desc': row[1][1], 'x': expansion_poly[i][0], 'y': expansion_poly[i][1]}, ignore_index=True)
 
     return df
 
+ENLARGE_PX = 15
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -101,7 +109,7 @@ if __name__ == '__main__':
     parser.add_argument("--images_dir", type=str, required=True)
     args = vars(parser.parse_args())
 
-    cleaned_dir = os.path.join(os.path.dirname(args['element_labels_dir']), 'element_labels_enlarged')
+    cleaned_dir = os.path.join(os.path.dirname(args['element_labels_dir']), f'element_labels_enlarged_{ENLARGE_PX}')
     makedirs(cleaned_dir, exist_ok=True)
 
     for path in glob(os.path.join(args['element_labels_dir'], '*')):
@@ -111,8 +119,7 @@ if __name__ == '__main__':
             img_path = os.path.join(args['images_dir'], visname + '.jpg')
 
         with Image.open(img_path) as im:
-
             element_labels = parse_element_label(os.path.join(args['element_labels_dir'], visname))
             element_labels = combine_rows(element_labels)
-            element_labels = enlargeAOI(element_labels, im, -0.05)
-            element_labels.to_csv(os.path.join(cleaned_dir, os.path.basename(path)), index=False)
+            element_labels = enlargeAOI(element_labels, im, ENLARGE_PX)
+            element_labels.to_csv(os.path.join(cleaned_dir, os.path.basename(path)), header=False, index=False)
